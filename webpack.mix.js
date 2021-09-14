f1bcee312a467e6b3ab3d7bce80ee75d8ce1f865
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-14 13:28:08 +0100

    Installed jetstream.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,15 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix
-  .js('resources/js/app.js', 'public/js')
-  .postCss('resources/css/app.css', 'public/css', [
-    //
-  ]);
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+    ]);
 
-mix.browserSync({
-  proxy: 'http://localhost:8000',
-  notify: false,
-});
-
-mix.disableNotifications();
+if (mix.inProduction()) {
+    mix.version();
+}
